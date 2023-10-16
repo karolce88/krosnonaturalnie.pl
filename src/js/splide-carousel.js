dd60487e66e7cd89bc1aed10706bcf1c94266170
Karol <karolcwiklik@o2.pl>
2023-10-16 21:30:54 +0200

    blog

--- src/js/splide-carousel.js
+++ src/js/splide-carousel.js
@@ -1,21 +1,37 @@
 document.addEventListener( 'DOMContentLoaded', function () {
     new Splide( '#image-carousel', {
-        perPage: 2,
+
         breakpoints: {
-            700: {
-				width: '100vw',
+            500: {
+                perPage: 1,
+            },
+			900: {
+				perPage: 2,
 			},
-			1000: {
-				perPage: 1,
+			1100: {
+				perPage: 3,
 			},
+			1275: {
+				perPage: 3,
+			},
+			1500: {
+				perPage: 4,
+                width: '100vw',
+			},
+			1660: {
+                width: '99vw',
+			},
+
 		},
         perMove: 1,
         gap    : '1rem',
-        padding: '3.5rem',
+        padding: '3rem',
         type: 'loop',
-        width: '70vw',
+        width: '93vw',
         drag: 'free',
         snap: true,
         autoplay: true,
+        interval: 2600,
+        perPage: 5,
     } ).mount();
   } );
